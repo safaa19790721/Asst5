df6e9eae298d394976e290e1dd121fc130a28fa2
safaa <safaa@192.168.0.4>
2018-10-31 04:58:54 -0400

    safaa2 , 1st comment

--- s4comp/src/MC.java
+++ s4comp/src/MC.java
@@ -3,7 +3,9 @@ public class MC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("message from safaa 4 safaa 19790271");
+		System.out.println("message from safaa 4 safaa 19790271");
+		System.out.println("message from safaa 2 safaa 19780271");
+
 	}
 
 }
