5a615685bdee4f6a9860080914d9ea6796455ef9
safaa <safaa@192.168.0.4>
2018-10-31 05:09:07 -0400

    safaa 2. 3rd change

--- s4comp/src/MC.java
+++ s4comp/src/MC.java
@@ -6,6 +6,8 @@ public class MC {
 		System.out.println("message from safaa 4 safaa 19790271");
 		System.out.println("message from safaa 2 safaa 19780271");
 		System.out.println("message2 from safaa 2 safaa 19780271");
+		int x=3+5;
+		
 
 	}
 
